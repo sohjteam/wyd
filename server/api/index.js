0ddb58d5a8c4d45d54603b7c5465686695636839
Dayoung Lee <dayoung31668@gmail.com>
2019-03-12 23:01:01 -0400

    Fixed/Added: user can now see their friends from a route that calls the friendship through table

--- server/api/index.js
+++ server/api/index.js
@@ -1,12 +1,14 @@
 const router = require('express').Router()
 
-router.use('/events', require('./events'))
-router.use('/groups', require('./groups'))
+// router.use('/events', require('./events'))
+// router.use('/groups', require('./groups'))
 router.use('/users', require('./users'))
-router.use('/notifications', require('./notifications'))
+// router.use('/notifications', require('./notifications'))
 
 router.use(next => {
   const err = new Error('Not Found')
   err.status(404)
   next(err)
 })
+
+module.exports = router
