c8385173fb9c662776b27855cd0a6094eb930675
Dayoung Lee <dayoung31668@gmail.com>
2019-03-16 13:35:58 -0400

    Added: update and delete to event routes'

--- server/api/events.js
+++ server/api/events.js
@@ -44,4 +44,37 @@ router.get('/:id/group', async (req, res, next) => {
   }
 })
 
+router.post('/', async (req, res, next) => {
+  try {
+    const newEvent = await Event.create(req.body)
+    res.json(newEvent)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.put('/:id', async (req, res, next) => {
+  try {
+    const eventId = req.params.id
+    let event = await Event.findById(eventId)
+    if (!event) throw next()
+    let updated = await event.update(req.body)
+    res.json(updated)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const eventId = req.params.id
+    const data = await Event.destroy({
+      where: {id: eventId}
+    })
+    res.json(data)
+  } catch (error) {
+    next(error)
+  }
+})
+
 module.exports = router
