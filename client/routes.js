3cf2090a6da390781470241869e36579dd95d701
Dayoung Lee <dayoung31668@gmail.com>
2019-03-28 18:46:15 -0400

    Removed console logs

--- client/routes.js
+++ client/routes.js
@@ -4,21 +4,21 @@ import {withRouter, Route, Switch} from 'react-router-dom'
 import {Login, UserHome, Signup} from './components'
 import {me} from './store'
 
-//add component did mount later
 class Routes extends Component {
   componentDidMount() {
     this.props.loadInitialData()
   }
   render() {
     const {isLoggedIn} = this.props
-    console.log('AYO', isLoggedIn)
     return (
       <>
         <Switch>
           <Route exact path="/login" component={Login} />
           <Route exact path="/signup" component={Signup} />
         </Switch>
+
         {isLoggedIn && (
+          //only avaliable after user logs in
           <Switch>
             <Route exact path="/userhome" component={UserHome} />
           </Switch>
