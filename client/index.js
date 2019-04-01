5c11a9bd18bde067a5a573dcc14af8833d5d616b
Rui Z <rrzhao13@gmail.com>
2019-04-01 17:06:36 -0400

    browser history added

--- client/index.js
+++ client/index.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {Provider} from 'react-redux'
-import {BrowserRouter as Router} from 'react-router-dom'
+import {Router} from 'react-router-dom'
+import history from './history'
 import store from './store'
 import App from './app'
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router>
+    <Router history={history}>
       <App />
     </Router>
   </Provider>,
