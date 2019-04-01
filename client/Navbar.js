5fd94be77835c53683f507f8b60f11480bed46b5
Rui Z <rrzhao13@gmail.com>
2019-04-01 17:17:53 -0400

    profile linked to userhome

--- client/Navbar.js
+++ client/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <nav id="navbar">
       WELCOME!
-      <Link to="/">Profile</Link>
+      <Link to="/userhome">Profile</Link>
       <Link to="/events">Events</Link>
       <Link to="/groups">Groups</Link>
       <Link to="/login">Login</Link>
