a8c8b3b3fc741755afb1ec9d481a2b77d6675ff9
Rui Z <rrzhao13@gmail.com>
2019-04-02 17:18:20 -0400

    styled navbar with reactstrap

--- client/Navbar.js
+++ client/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {Component} from 'react'
 import {
   Collapse,
   Navbar,
@@ -14,33 +14,64 @@ import {
 } from 'reactstrap'
 
 const barStyle = {
-  backgroundColor: '#b7d1e1'
+  backgroundColor: '#b7d1e1',
+  fontFamily: ''
 }
 
 const linkStyle = {
   color: 'white'
 }
 
-const NavigationBar = () => {
-  return (
-    <div>
-      <Navbar style={barStyle} light expand="md">
-        <NavbarBrand href="/">reactstrap</NavbarBrand>
-        <Nav className="ml-auto" navbar>
-          <NavItem>
-            <NavLink style={linkStyle} href="/components/">
-              Components
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href="https://github.com/reactstrap/reactstrap">
-              GitHub
-            </NavLink>
-          </NavItem>
-        </Nav>
-      </Navbar>
-    </div>
-  )
+class NavigationBar extends Component {
+  constructor(props) {
+    super(props)
+
+    this.toggle = this.toggle.bind(this)
+    this.state = {
+      isOpen: false
+    }
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    })
+  }
+  render() {
+    return (
+      <div>
+        <Navbar style={barStyle} light expand="md">
+          <NavbarBrand href="/">
+            <img src="wydWeb.png" width="100" height="40" />
+          </NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink style={linkStyle} href="/profile">
+                  Profile
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink style={linkStyle} href="/events">
+                  Events
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink style={linkStyle} href="/groups">
+                  Groups
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink style={linkStyle} href="/login">
+                  Login
+                </NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    )
+  }
 }
 
 export default NavigationBar
