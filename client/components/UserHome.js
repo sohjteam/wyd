4fb48dc0eaf13d81c454d167479e01616102cda1
Dayoung Lee <dayoung31668@gmail.com>
2019-04-05 22:39:15 -0400

    Added: defauly user image for userhome

--- client/components/UserHome.js
+++ client/components/UserHome.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {getMyEvents} from '../store/events'
+import {me} from '../store/user'
+import {Container, Row, Col} from 'reactstrap'
 
 class UserHome extends Component {
   componentDidMount() {
@@ -11,15 +13,33 @@ class UserHome extends Component {
     if (!this.props.myEvents) {
       this.props.myEvents = []
     }
+    let user = this.props.user
     return (
       <>
-        <h1>My Events</h1>
-        {this.props.myEvents.map(event => (
-          <li key={event.id}>
-            <h1>Event: {event.name}</h1>
-            <h1>Event Type: {event.type}</h1>
-          </li>
-        ))}
+        <Container>
+          <Row>
+            <div id="profile">
+              <Col xs="3">Profile</Col>
+              <img src={user.image} width="150" />
+              <h1>
+                {' '}
+                Name: {user.firstName} {user.lastName}
+              </h1>
+              <h1>Email: {user.email}</h1>
+            </div>
+            <div id="userEvents">
+              <Col xs="auto">
+                My Events
+                {this.props.myEvents.map(event => (
+                  <div key={event.id}>
+                    <h1>Event: {event.name}</h1>
+                    <h1>Event Type: {event.type}</h1>
+                  </div>
+                ))}
+              </Col>
+            </div>
+          </Row>
+        </Container>
       </>
     )
   }
@@ -27,10 +47,12 @@ class UserHome extends Component {
 
 const mapStateToProps = state => ({
   userId: state.user.id,
-  myEvents: state.events.myEvents
+  myEvents: state.events.myEvents,
+  user: state.user
 })
 const mapDispatchToProps = dispatch => ({
-  getMyEvents: userId => dispatch(getMyEvents(userId))
+  getMyEvents: userId => dispatch(getMyEvents(userId)),
+  me: () => dispatch(me())
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(UserHome)
