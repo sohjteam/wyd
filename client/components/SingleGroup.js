d931f5163e760611668e4986e7d5a545b5ccaaaf
Rui Z <rrzhao13@gmail.com>
2019-04-10 15:35:14 -0400

    events added to group calender

--- client/components/SingleGroup.js
+++ client/components/SingleGroup.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
+import {getGroupEvents} from '../store/events'
 import Calendar from 'react-big-calendar'
 import moment from 'moment'
 import 'react-big-calendar/lib/css/react-big-calendar.css'
@@ -9,15 +10,20 @@ const localizer = Calendar.momentLocalizer(moment)
 class SingleGroup extends Component {
   constructor() {
     super()
-    this.state = {
-      events: [
-        {
-          start: new Date(),
-          end: new Date(moment().add(1, 'days')),
-          title: 'Some title'
-        }
-      ]
-    }
+    // this.state = {
+    //   events: [
+    //     {
+    //       start: new Date(),
+    //       end: new Date(moment().add(1, 'days')),
+    //       title: 'Some title'
+    //     }
+    //   ]
+    // }
+  }
+
+  componentDidMount() {
+    const groupId = this.props.match.params.id
+    this.props.getEvents(groupId)
   }
 
   render() {
@@ -26,11 +32,23 @@ class SingleGroup extends Component {
         localizer={localizer}
         defaultDate={new Date()}
         defaultView="month"
-        events={this.state.events}
+        events={this.props.events.map(event => ({
+          start: event.startDate,
+          end: event.startDate,
+          title: event.name
+        }))}
         style={{height: '100vh'}}
       />
     )
   }
 }
 
-export default SingleGroup
+const mapStateToProps = state => ({
+  events: state.events.groupEvents
+})
+
+const mapDispatchToProps = dispatch => ({
+  getEvents: groupId => dispatch(getGroupEvents(groupId))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(SingleGroup)
