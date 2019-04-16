4688ac3e463018322818e450672fb8e1b2d36f8f
Rui Z <rrzhao13@gmail.com>
2019-04-16 17:08:58 -0400

    added group name, image and members to single group page

--- client/components/SingleGroup.js
+++ client/components/SingleGroup.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {getGroupEvents} from '../store/events'
+import {getGroup} from '../store/groups'
 import Calendar from 'react-big-calendar'
 import moment from 'moment'
 import 'react-big-calendar/lib/css/react-big-calendar.css'
@@ -8,47 +9,57 @@ import 'react-big-calendar/lib/css/react-big-calendar.css'
 const localizer = Calendar.momentLocalizer(moment)
 
 class SingleGroup extends Component {
-  constructor() {
-    super()
-    // this.state = {
-    //   events: [
-    //     {
-    //       start: new Date(),
-    //       end: new Date(moment().add(1, 'days')),
-    //       title: 'Some title'
-    //     }
-    //   ]
-    // }
-  }
-
   componentDidMount() {
     const groupId = this.props.match.params.id
+    this.props.getGroup(groupId)
     this.props.getEvents(groupId)
   }
 
   render() {
+    const group = this.props.group
+    const members = this.props.members
     return (
-      <Calendar
-        localizer={localizer}
-        defaultDate={new Date()}
-        defaultView="month"
-        events={this.props.events.map(event => ({
-          start: event.startDate,
-          end: event.startDate,
-          title: event.name
-        }))}
-        style={{height: '100vh'}}
-      />
+      <>
+        <>
+          <img src={group.image} width="150" />
+          <h5>{group.name}</h5>
+          <p>Members: </p>
+          <ul>
+            {members.map(member => (
+              <li>
+                <img src={member.image} />
+                {member.firstName} {member.lastName}
+              </li>
+            ))}
+          </ul>
+        </>
+        <>
+          <Calendar
+            localizer={localizer}
+            defaultDate={new Date()}
+            defaultView="month"
+            events={this.props.events.map(event => ({
+              start: event.startDate,
+              end: event.startDate,
+              title: event.name
+            }))}
+            style={{height: '80vh', width: '70vw'}}
+          />
+        </>
+      </>
     )
   }
 }
 
 const mapStateToProps = state => ({
-  events: state.events.groupEvents
+  events: state.events.groupEvents,
+  group: state.groups.group,
+  members: state.groups.members
 })
 
 const mapDispatchToProps = dispatch => ({
-  getEvents: groupId => dispatch(getGroupEvents(groupId))
+  getEvents: groupId => dispatch(getGroupEvents(groupId)),
+  getGroup: groupId => dispatch(getGroup(groupId))
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(SingleGroup)
--- client/store/groups.js
+++ client/store/groups.js
@@ -1,11 +1,15 @@
 import axios from 'axios'
 
 const GET_MY_GROUPS = 'GET_MY_GROUPS'
+const GET_GROUP = 'GET_MEMBERS'
 
 const gotMyGroups = groups => ({type: GET_MY_GROUPS, groups})
+const gotGroup = group => ({type: GET_GROUP, group})
 
 const initialState = {
-  groups: []
+  groups: [],
+  group: {},
+  members: []
 }
 
 export const getMyGroups = userId => async dispatch => {
@@ -17,10 +21,21 @@ export const getMyGroups = userId => async dispatch => {
   }
 }
 
+export const getGroup = groupId => async dispatch => {
+  try {
+    const res = await axios.get(`/api/groups/${groupId}/members`)
+    dispatch(gotGroup(res.data))
+  } catch (error) {
+    console.log(error)
+  }
+}
+
 export default function(state = initialState, action) {
   switch (action.type) {
     case GET_MY_GROUPS:
       return {...state, groups: action.groups}
+    case GET_GROUP:
+      return {...state, group: action.group, members: action.group.users}
     default:
       return state
   }
