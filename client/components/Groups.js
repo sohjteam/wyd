2184236b7d40bac95d666bd84664a54bcb82ea8e
Rui Z <rrzhao13@gmail.com>
2019-04-19 01:31:03 -0400

    moved add event to single group

--- client/components/Groups.js
+++ client/components/Groups.js
@@ -2,28 +2,17 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 import {getMyGroups} from '../store/groups'
-import {Container, Col, Button, Modal, ModalHeader, ModalBody} from 'reactstrap'
-import EventForm from './EventForm'
+import {Container, Col} from 'reactstrap'
 
 class Groups extends Component {
   constructor(props) {
     super(props)
-    this.state = {
-      modal: false
-    }
-
-    this.toggle = this.toggle.bind(this)
   }
 
   componentDidMount() {
     this.props.getMyGroups(this.props.userId)
   }
 
-  toggle() {
-    this.setState(prevState => ({
-      modal: !prevState.modal
-    }))
-  }
   render() {
     if (!this.props.myGroups) {
       this.props.myGroups = []
@@ -39,15 +28,6 @@ class Groups extends Component {
                 <img src={group.image} width="150" />
                 <p>{group.name}</p>
               </Link>
-
-              <Button onClick={this.toggle}>
-                <Modal isOpen={this.state.modal} toggle={this.toggle}>
-                  <ModalHeader toggle={this.toggle}>Add An Event!</ModalHeader>
-                  <ModalBody>
-                    <EventForm groupId={group.id} />
-                  </ModalBody>
-                </Modal>
-              </Button>
             </div>
           ))}
         </Col>
--- client/components/SingleGroup.js
+++ client/components/SingleGroup.js
@@ -2,7 +2,16 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {getGroupEvents} from '../store/events'
 import {getSingleGroup} from '../store/groups'
-import {Container, Col, Row} from 'reactstrap'
+import EventForm from './EventForm'
+import {
+  Container,
+  Col,
+  Row,
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody
+} from 'reactstrap'
 import Calendar from 'react-big-calendar'
 import moment from 'moment'
 import 'react-big-calendar/lib/css/react-big-calendar.css'
@@ -22,12 +31,23 @@ const localizer = Calendar.momentLocalizer(moment)
 class SingleGroup extends Component {
   constructor() {
     super()
+    this.state = {
+      modal: false
+    }
+
+    this.toggle = this.toggle.bind(this)
   }
 
   componentDidMount() {
     const groupId = this.props.match.params.id
     this.props.getGroup(groupId)
     this.props.getEvents(groupId)
+  }
+
+  toggle() {
+    this.setState(prevState => ({
+      modal: !prevState.modal
+    }))
   }
 
   render() {
@@ -38,8 +58,17 @@ class SingleGroup extends Component {
         <Container fluid>
           <Row>
             <Col sm="3" style={memStyle}>
-              <img src={window.location.origin + group.image} height="150" />
+              <img src={group.image} height="150" />
               <h1 className="title">{group.name}</h1>
+              <Button onClick={this.toggle}>
+                Add Event
+                <Modal isOpen={this.state.modal} toggle={this.toggle}>
+                  <ModalHeader toggle={this.toggle}>Add An Event!</ModalHeader>
+                  <ModalBody>
+                    <EventForm groupId={group.id} />
+                  </ModalBody>
+                </Modal>
+              </Button>
               <h3>Members: </h3>
               {members.map(member => (
                 <li>
