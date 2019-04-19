e0ee9a33502e206023088d238d62afd9a189feb6
Rui Z <rrzhao13@gmail.com>
2019-04-19 00:32:45 -0400

    group name and members added but images don't load

--- client/components/SingleGroup.js
+++ client/components/SingleGroup.js
@@ -1,54 +1,82 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {getGroupEvents} from '../store/events'
+import {getSingleGroup} from '../store/groups'
+import {Container, Col, Row} from 'reactstrap'
 import Calendar from 'react-big-calendar'
 import moment from 'moment'
 import 'react-big-calendar/lib/css/react-big-calendar.css'
 
+const CalStyle = {
+  paddingTop: 20,
+  paddingRight: 30,
+  PaddingLeft: 20
+}
+
+const memStyle = {
+  paddingTop: 20,
+  PaddingLeft: 50
+}
 const localizer = Calendar.momentLocalizer(moment)
 
 class SingleGroup extends Component {
   constructor() {
     super()
-    // this.state = {
-    //   events: [
-    //     {
-    //       start: new Date(),
-    //       end: new Date(moment().add(1, 'days')),
-    //       title: 'Some title'
-    //     }
-    //   ]
-    // }
   }
 
   componentDidMount() {
     const groupId = this.props.match.params.id
+    this.props.getGroup(groupId)
     this.props.getEvents(groupId)
   }
 
   render() {
+    const group = this.props.group
+    const members = this.props.members
     return (
-      <Calendar
-        localizer={localizer}
-        defaultDate={new Date()}
-        defaultView="month"
-        events={this.props.events.map(event => ({
-          start: event.startDate,
-          end: event.startDate,
-          title: event.name
-        }))}
-        style={{height: '100vh'}}
-      />
+      <>
+        <Container fluid>
+          <Row>
+            <Col sm="3" style={memStyle}>
+              <img src={window.location.origin + group.image} height="150" />
+              <h1 className="title">{group.name}</h1>
+              <h3>Members: </h3>
+              {members.map(member => (
+                <li>
+                  <img src={member.image} height="150" />
+                  {member.firstName} {member.lastName}
+                </li>
+              ))}
+            </Col>
+            <Col style={CalStyle}>
+              <Calendar
+                localizer={localizer}
+                defaultDate={new Date()}
+                defaultView="month"
+                events={this.props.events.map(event => ({
+                  start: event.startDate,
+                  end: event.startDate,
+                  title: event.name
+                }))}
+                style={{height: '80vh'}}
+              />
+            </Col>
+          </Row>
+        </Container>
+      </>
     )
   }
 }
 
 const mapStateToProps = state => ({
-  events: state.events.groupEvents
+  events: state.events.groupEvents,
+  group: state.groups.group,
+  members: state.groups.members
 })
 
 const mapDispatchToProps = dispatch => ({
-  getEvents: groupId => dispatch(getGroupEvents(groupId))
+  getEvents: groupId => dispatch(getGroupEvents(groupId)),
+  getGroup: groupId => dispatch(getSingleGroup(groupId))
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(SingleGroup)
--- client/store/groups.js
+++ client/store/groups.js
@@ -1,11 +1,15 @@
 import axios from 'axios'
 
 const GET_MY_GROUPS = 'GET_MY_GROUPS'
+const GET_SINGLE_GROUP = 'GET_SINGLE_GROUP'
 
 const gotMyGroups = groups => ({type: GET_MY_GROUPS, groups})
+const gotSingleGroup = group => ({type: GET_SINGLE_GROUP, group})
 
 const initialState = {
-  groups: []
+  groups: [],
+  group: {},
+  members: []
 }
 
 export const getMyGroups = userId => async dispatch => {
@@ -17,10 +21,21 @@ export const getMyGroups = userId => async dispatch => {
   }
 }
 
+export const getSingleGroup = groupId => async dispatch => {
+  try {
+    const res = await axios.get(`/api/groups/${groupId}/members`)
+    dispatch(gotSingleGroup(res.data))
+  } catch (error) {
+    console.log(error)
+  }
+}
+
 export default function(state = initialState, action) {
   switch (action.type) {
     case GET_MY_GROUPS:
       return {...state, groups: action.groups}
+    case GET_SINGLE_GROUP:
+      return {...state, group: action.group, members: action.group.users}
     default:
       return state
   }
