da83d810384c2ba98f8b8ff4e1c1d3745ae78fc2
Dayoung Lee <dayoung31668@gmail.com>
2019-04-23 16:58:10 -0400

    Added: groupform

--- client/components/GroupForm.js
+++ client/components/GroupForm.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
+import {addNewGroup} from '../store/groups'
+import {getMyFriends} from '../store/user'
 import {Form, FormGroup, Label, Input, Button} from 'reactstrap'
 
 class GroupForm extends Component {
@@ -13,6 +15,9 @@ class GroupForm extends Component {
     }
     this.handleSubmit = this.handleSubmit.bind(this)
     this.handleChange = this.handleChange.bind(this)
+  }
+  componentDidMount() {
+    this.props.getMyFriends(this.props.userId)
   }
   handleChange = evt => {
     this.setState({
@@ -29,14 +34,75 @@ class GroupForm extends Component {
       image,
       members
     }
+    this.props.addNewGroup(newGroup)
   }
   render() {
-    return <></>
+    if (!this.props.groups) {
+      this.props.groups = []
+    }
+    if (!this.props.userFriends) {
+      this.props.userFriends = []
+    }
+    return (
+      <>
+        <Form id="groupForm" onSubmit={this.handleSubmit}>
+          <FormGroup>
+            <Label for="groupName">Group Name</Label>
+            <Input
+              id="groupNameText"
+              placeholder="Group Name"
+              name="name"
+              onChange={this.handleChange}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label for="groupPassword">Group Password</Label>
+            <Input
+              id="groupPasswordText"
+              placeholder="Group Password"
+              type="password"
+              name="password"
+              onChange={this.handleChange}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label for="groupImage">Group Image</Label>
+            <Input
+              id="groupImageText"
+              type="file"
+              name="image"
+              onChange={this.handleChange}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label for="exampleSelectMulti">Select Multiple</Label>
+            <Input
+              type="select"
+              name="members"
+              id="groupMembersSelect"
+              multiple
+            >
+              {this.props.userFriends.map(friend => (
+                <option key={friend.id}>{friend.firstName}</option>
+              ))}
+            </Input>
+          </FormGroup>
+          <Button type="submit">Submit</Button>
+        </Form>
+      </>
+    )
   }
 }
 
-const mapStateToProps = state => ({})
+const mapStateToProps = state => ({
+  userId: state.user.user.id,
+  groups: state.groups.groups,
+  userFriends: state.user.friends
+})
 
-const mapDispatchToProps = dispatch => ({})
+const mapDispatchToProps = dispatch => ({
+  addNewGroup: newGroup => dispatch(addNewGroup(newGroup)),
+  getMyFriends: userId => dispatch(getMyFriends(userId))
+})
 
 export default connect(mapStateToProps, mapDispatchToProps)(GroupForm)
