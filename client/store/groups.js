cd978d57c76fb83d3f8850ea5c55ff41e17063f5
Dayoung Lee <dayoung31668@gmail.com>
2019-04-23 16:22:05 -0400

    Created thunk, action creator and added to store for new group

--- client/store/groups.js
+++ client/store/groups.js
@@ -2,9 +2,11 @@ import axios from 'axios'
 
 const GET_MY_GROUPS = 'GET_MY_GROUPS'
 const GET_SINGLE_GROUP = 'GET_SINGLE_GROUP'
+const ADD_NEW_GROUP = 'ADD_NEW_GROUP'
 
 const gotMyGroups = groups => ({type: GET_MY_GROUPS, groups})
 const gotSingleGroup = group => ({type: GET_SINGLE_GROUP, group})
+const addedGroup = newGroup => ({type: ADD_NEW_GROUP, newGroup})
 
 const initialState = {
   groups: [],
@@ -30,12 +32,23 @@ export const getSingleGroup = groupId => async dispatch => {
   }
 }
 
+export const addNewGroup = newGroup => async dispatch => {
+  try {
+    const res = await axios.post('/api/groups/', newGroup)
+    dispatch(addedGroup(res.data))
+  } catch (error) {
+    console.log(error)
+  }
+}
+
 export default function(state = initialState, action) {
   switch (action.type) {
     case GET_MY_GROUPS:
       return {...state, groups: action.groups}
     case GET_SINGLE_GROUP:
       return {...state, group: action.group, members: action.group.users}
+    case ADD_NEW_GROUP:
+      return {...state, groups: [...state.groups, action.newGroup]}
     default:
       return state
   }
