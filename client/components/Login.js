b076683d802c7f0b84d0b9b37911d1e8d48781b1
Rui Z <rrzhao13@gmail.com>
2019-04-23 02:52:23 -0400

    login finished

--- client/components/Login.js
+++ client/components/Login.js
@@ -2,7 +2,17 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {auth} from '../store/user'
 import {Link} from 'react-router-dom'
-import {Form, FormGroup, Label, Col, Input, Button} from 'reactstrap'
+import {Container, Form, FormGroup, Label, Col, Input, Button} from 'reactstrap'
+
+const conStyle = {
+  marginTop: '20vh',
+  width: '40vw'
+}
+
+const buttonStyle = {
+  marginRight: 20,
+  width: 100
+}
 
 class Login extends Component {
   constructor() {
@@ -29,42 +39,49 @@ class Login extends Component {
 
   render() {
     return (
-      <Form onSubmit={this.handleSubmit}>
-        <FormGroup row>
-          <Label for="email" sm={2}>
-            Email
-          </Label>
-          <Col sm={10}>
-            <Input
-              type="email"
-              name="email"
-              id="email"
-              placeholder=""
-              onChange={this.handleChange}
-              value={this.state.email}
-            />
+      <Container style={conStyle}>
+        <Form onSubmit={this.handleSubmit}>
+          <Col md={{size: 6, offset: 5}}>
+            <h2>Login</h2>
           </Col>
-        </FormGroup>
-        <FormGroup row>
-          <Label for="password" sm={2}>
-            Password
-          </Label>
-          <Col sm={10}>
-            <Input
-              type="password"
-              name="password"
-              id="password"
-              placeholder=""
-              onChange={this.handleChange}
-              value={this.state.password}
-            />
+          <FormGroup row>
+            <Label for="email" sm={2}>
+              Email
+            </Label>
+            <Col sm={10}>
+              <Input
+                type="email"
+                name="email"
+                id="email"
+                placeholder=""
+                onChange={this.handleChange}
+                value={this.state.email}
+              />
+            </Col>
+          </FormGroup>
+          <FormGroup row>
+            <Label for="password" sm={2}>
+              Password
+            </Label>
+            <Col sm={10}>
+              <Input
+                type="password"
+                name="password"
+                id="password"
+                placeholder=""
+                onChange={this.handleChange}
+                value={this.state.password}
+              />
+            </Col>
+          </FormGroup>
+          <Col md={{size: 12, offset: 3}}>
+            <Button style={buttonStyle}>Submit</Button>
+            <Link to="/signup">
+              <Button>Create an Account</Button>
+            </Link>
           </Col>
-        </FormGroup>
-        <Button>Submit</Button>
-        <Link to="/signup">
-          <Button>Create an Account</Button>
-        </Link>
-      </Form>
+        </Form>
+      </Container>
     )
   }
 }
